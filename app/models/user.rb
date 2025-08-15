b41a4cb10590dfdd70e1eaaf7ef15c7c42b162a9
taniguuuuuuuu <miiiisaaaakiiii.920616@gmail.com>
2025-08-15 21:37:09 +0900

    ヘッダーの表示「こんにちは、〇〇さん」の表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   validates :name,        presence: true
   validates :profile,     presence: true
-  validates :occupation, presence: true
+  validates :occupation,  presence: true
   validates :position,    presence: true
 end
