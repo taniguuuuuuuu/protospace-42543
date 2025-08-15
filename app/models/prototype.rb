f7a412e98990c36dc301bc344503390c017808b7
taniguuuuuuuu <miiiisaaaakiiii.920616@gmail.com>
2025-08-16 00:34:25 +0900

    適切なビューファイルを設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,13 @@
 class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :title,      presence: true
+  validates :catch_copy, presence: true
+  validates :concept,    presence: true
+
+  validates :image,
+           attached: true,
+           content_type: %w[image/png image/jpg image/jpeg],
+           size: { less_than: 5.megabytes }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "prototypes#index"
+
+  resources :prototypes, only: [:new, :create]
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
