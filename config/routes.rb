68a6607e73f3a778c31570c6bf52bdefdf5cd175
taniguuuuuuuu <miiiisaaaakiiii.920616@gmail.com>
2025-08-19 16:10:00 +0900

    アクションとルーティングを設定しましょう

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "prototypes#index"
 
-  resources :prototypes, only: [:new, :create, :show, :edit, :update]
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
