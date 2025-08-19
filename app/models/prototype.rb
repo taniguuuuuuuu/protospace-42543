6479c87abfed8778b4d0bc79f49817ef568c585c
taniguuuuuuuu <miiiisaaaakiiii.920616@gmail.com>
2025-08-19 16:49:00 +0900

    Commentモデルおよびテーブルを作成しましょう

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,7 @@
 class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  # has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   with_options presence: true do
     validates :title, :catch_copy, :concept
