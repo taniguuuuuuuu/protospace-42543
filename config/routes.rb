578df2e6fd1e4af2d189d6e4c4b0898684015a94
taniguuuuuuuu <miiiisaaaakiiii.920616@gmail.com>
2025-08-26 19:51:16 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :comments, only: :create
   resources :users,      only: :show
   
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
