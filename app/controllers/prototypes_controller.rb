b446fc8b51be83e294fed94c81e41397cbd79b12
taniguuuuuuuu <miiiisaaaakiiii.920616@gmail.com>
2025-08-26 17:31:52 +0900

    ログインしていないユーザーのページ遷移を制限しましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,6 +2,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
     @prototypes = Prototype.includes(:user).with_attached_image.order(created_at: :desc)
   end
