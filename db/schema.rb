8d3ab18b72cd2e017d3a2a709885653839d16570
taniguuuuuuuu <miiiisaaaakiiii.920616@gmail.com>
2025-08-29 21:03:13 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -40,9 +40,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_19_072530) do
   end
 
   create_table "comments", charset: "utf8mb3", force: :cascade do |t|
-    t.text "content", null: false
     t.bigint "user_id", null: false
     t.bigint "prototype_id", null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["prototype_id"], name: "index_comments_on_prototype_id"
